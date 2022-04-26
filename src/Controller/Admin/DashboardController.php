cde04dc19e4df6af3aaae7f69295cfd64270a5c1
evgen1067 <bogomolov.evgen1067@mail.ru>
2022-04-26 16:50:38 +0300

    выход с админки

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,10 +9,12 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -37,12 +39,13 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard (): Dashboard
     {
         return Dashboard::new()
-            // the name visible to end users
             ->setTitle('QAservices.');
     }
 
     public function configureMenuItems (): iterable
     {
+        yield MenuItem::linkToUrl('Назад', 'fas fa-home', '/');
+
         yield MenuItem::subMenu('Пользователи', 'fas fa-bars')
             ->setSubItems([
                     MenuItem::linkToCrud(
@@ -97,7 +100,5 @@ class DashboardController extends AbstractDashboardController
                     )
                 ]
             );
-
-
     }
 }
